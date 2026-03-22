48a97f5935808c8408b31d209a59f731f5bf3dd0
Pallav Mishra <pallavmishra@TI-MAC-152.local>
2026-03-22 17:52:56 +0530

    updated logging config

--- hybrid_orienter/__init__.py
+++ hybrid_orienter/__init__.py
@@ -1,3 +1,14 @@
+import logging
+import sys
+
+# Configure package logger to write to stdout with timestamps
+_logger = logging.getLogger("hybrid_orienter")
+if not _logger.handlers:
+    _handler = logging.StreamHandler(sys.stdout)
+    _handler.setFormatter(logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s"))
+    _logger.addHandler(_handler)
+    _logger.setLevel(logging.INFO)
+
 from .orienter import HybridOrienter
 from .config import HybridOrienterConfig, SuryaPriorConfig
 from .surya_prior import SuryaPrior
